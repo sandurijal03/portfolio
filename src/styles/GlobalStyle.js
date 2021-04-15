5285f4e6d1a559f2e977f9895c4c2f7e19db3013
Sandip Rijal <sandurijal03@hotmail.com>
2021-04-16 01:31:26 +0545

    Adding files and folders for the project

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -1,14 +1,20 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyled = createGlobalStyle`
+  :root {
+    --background-color: #000;
+  }
+
+
   * {
     padding:0;
     margin:0;
     box-sizing:border-box;
-    font-size: 1.2rem;
+    list-style: none;
+    text-decoration:none;
   }
   body {
-    background-color: #333;
+    background-color: var(--background-color)
   }
 `;
 
