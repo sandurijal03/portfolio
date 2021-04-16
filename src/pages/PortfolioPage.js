8cbebc28b50a6c10280cd16fae8f2adff736225c
Sandip Rijal <sandurijal03@hotmail.com>
2021-04-17 03:40:41 +0545

    finishing portfolio section

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Title from '../components/Title';
+import { InnerLayout, MainLayout } from '../styles/Layouts';
+
+import portfolios from '../data/portfolios';
+import Menu from '../components/Menu';
+import Button from '../components/Button';
+
+const allButtons = ['All', ...new Set(portfolios.map((item) => item.category))];
 
 const PortfolioPage = () => {
+  const [menuItem, setMenuItems] = useState(portfolios);
+  const [button, setButtons] = useState(allButtons);
+
+  const filter = (button) => {
+    if (button === 'All') {
+      setMenuItems(portfolios);
+      return;
+    }
+
+    const filteredData = portfolios.filter((item) => item.category === button);
+    setMenuItems(filteredData);
+  };
+
   return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
+    <MainLayout>
+      <Title title={'Portfolio'} span={'Portfolio'} />
+      <InnerLayout>
+        <Button filter={filter} button={button} />
+        <Menu menuItem={menuItem} />
+      </InnerLayout>
+    </MainLayout>
   );
 };
 
